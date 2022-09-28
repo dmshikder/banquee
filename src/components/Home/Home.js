0d63692f04a05c5cb84cf75f705e9d0c75a91489
dmshikder <dmshikder@gmail.com>
2022-09-28 13:09:00 +0600

    card section created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import AllFeatures from '../AllFeatures/AllFeatures'
-import Features from '../Features/Features'
-import Hero from '../Hero/Hero'
-import './Home.css'
+import React from 'react';
+import AllFeatures from '../AllFeatures/AllFeatures';
+import Cards from '../Cards/Cards';
+import Features from '../Features/Features';
+import Hero from '../Hero/Hero';
+import './Home.css';
 const Home = () => {
   return (
     <div className='home'>
         <Hero/>
         <Features/>
+        <hr className='line'/>
         <AllFeatures/>
+        <Cards/>
     </div>
   )
 }
